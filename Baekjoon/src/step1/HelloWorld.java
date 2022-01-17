afec1adf58777de416aecafc6b5f28cdc2b5d3ff
jinny <dms873@naver.com>
2022-01-17 16:10:36 +0900

    Solved : 220117

---
+++ Baekjoon/src/step1/HelloWorld.java
@@ -0,0 +1,21 @@
+package step1;
+
+public class HelloWorld {
+	
+	/**
+	* ABOUT
+	*
+	* DATE: 2022-01-17
+	* AUTHOR: dms873
+	*
+	* URL: https://www.acmicpc.net/problem/2557
+	*
+	* COMMENT:
+	* 출력 메소드를 사용함.
+	**/
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
+
---
+++ Baekjoon/src/step1/WeLoveKriii.java
@@ -0,0 +1,21 @@
+package step1;
+
+public class WeLoveKriii {
+	
+	/**
+	* ABOUT
+	*
+	* DATE: 2022-01-17
+	* AUTHOR: dms873
+	*
+	* URL: https://www.acmicpc.net/problem/10718
+	*
+	* COMMENT:
+	* 출력메소드 사용
+	**/
+	
+	public static void main(String[] args) {
+        System.out.println("강한친구 대한육군"); 
+        System.out.println("강한친구 대한육군"); 
+	}
+}
