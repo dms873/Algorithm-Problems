9cd13286b47732a12e59b86fae3d6cb714f4380e
jinny <dms873@naver.com>
2022-01-18 00:55:07 +0900

    Solved : 220118

---
+++ Baekjoon/src/step1/개.java
@@ -0,0 +1,24 @@
+package step1;
+
+public class 개 {
+	public static void main(String[] args) {
+		/**
+		* ABOUT
+		*
+		* DATE: 2022-01-18
+		* AUTHOR: dms873
+		*
+		* URL: https://www.acmicpc.net/problem/10172
+		*
+		* COMMENT:
+		* Escape Sequence 사용
+		**/
+		
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+        
+	}
+}
---
+++ Baekjoon/src/step1/고양이.java
@@ -0,0 +1,24 @@
+package step1;
+
+public class 고양이 {
+	public static void main(String[] args) {
+		/**
+		 * ABOUT
+		 *
+		 * DATE: 2022-01-18
+		 * AUTHOR: dms873
+		 *
+		 * URL: https://www.acmicpc.net/problem/10171
+		 *
+		 * COMMENT:
+		 * 자바에서는 몇몇 문자는 단독으로 써서 출력할 수 없다. ( 대표적으로 백슬래시와 큰 따옴표가 있다. )
+		 * 그래서 문자를 출력하기 위해 조합을 만들어서 출력할 수 있도록 하는데 이를 Escape Sequence 라고 한다.
+		 * 이스케이프 시퀀스는 백슬래시() + 문자 의 조합으로 쓰인다.
+		 **/
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
