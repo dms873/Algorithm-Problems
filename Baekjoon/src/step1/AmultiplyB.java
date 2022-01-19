e19f69c6eac3626be09483fa789727b4d7efbecb
jinny <dms873@naver.com>
2022-01-20 08:56:53 +0900

    Solved : 220120

---
+++ Baekjoon/src/step1/AmultiplyB.java
@@ -0,0 +1,26 @@
+package step1;
+
+import java.util.Scanner;
+
+public class AmultiplyB {
+	/**
+	 * ABOUT
+	 *
+	 * DATE: 2022-01-20
+	 * AUTHOR: dms873
+	 *
+	 * URL: https://www.acmicpc.net/problem/10998
+	 *
+	 * COMMENT:
+	 * Scanner를 사용하여 정수를 입력받아 곱해줌
+	 **/
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		System.out.println(A * B);
+		sc.close();
+	}
+}
