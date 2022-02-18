99228196d9ee1032c50720570e02796e2e25ebe3
Jinny <83773369+dms873@users.noreply.github.com>
2022-02-18 22:43:35 +0900

    Solved : 220218

---
+++ Programmers/src/step1/문자열을 정수로 바꾸기.js
@@ -0,0 +1,18 @@
+	/**
+	* ABOUT
+	*
+	* DATE: 2022-02-18
+	* AUTHOR: dms873
+	*
+	* URL: https://programmers.co.kr/learn/courses/30/lessons/12925
+	*
+	* COMMENT: String s를 Number()타입으로 바꿔주고 ParseInt()를 사용하여 answer에 담아준다.
+	* 
+	**/
+
+function solution(s) {
+    var answer = 0;
+    let num = Number(s)
+    
+    return answer = parseInt(num);
+}
