1ab722956734040ebbc6568f9eb7de61c27128f2
Jinny <83773369+dms873@users.noreply.github.com>
2022-02-19 09:55:55 +0900

    Solved : 220219

---
+++ Programmers/src/step1/StringInt.java
@@ -0,0 +1,19 @@
+	/**
+	* ABOUT
+	*
+	* DATE: 2022-02-19
+	* AUTHOR: dms873
+	*
+	* URL: https://programmers.co.kr/learn/courses/30/lessons/12925
+	*
+	* COMMENT: String s를 숫자로 바꿔주는 Integer.parseInt()를 사용하여 answer에 담아준다.
+	* 
+	**/
+  
+  class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
